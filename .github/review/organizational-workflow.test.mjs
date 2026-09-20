832c5f5722358591bc63c2adea812a064f3dabf5
German Abrikosov <etogerman@gmail.com>
2026-09-20 03:39:31 +0300

    Закрепить исправленный контроллер DeepSeek в общем подключении

--- .github/review/organizational-workflow.test.mjs
+++ .github/review/organizational-workflow.test.mjs
@@ -19,7 +19,7 @@ const organizationCaller = readFileSync(".github/workflows/review-all-trigger.ym
 const contributing = readFileSync("CONTRIBUTING.md", "utf8");
 const pullRequestTemplate = readFileSync(".github/pull_request_template.md", "utf8");
 const reviewedWorkflowSha = "ce8a887cbb97fd01afcc65384d34046431613dd9";
-const organizationWorkflowSha = "1dfb06106c7263ce1bd2cc17815a283341f8d30f";
+const organizationWorkflowSha = "86f26d64bb986b0c1982c88690ce0178e68c74ed";
 const emptyManifestHash = "4f53cda18c2baa0c0354bb5f9a3ecbe5ed12ab4d8e11ba873c2f11161202b945";
 
 function binaryCoverageMarker(files = 0, hash = emptyManifestHash) {
